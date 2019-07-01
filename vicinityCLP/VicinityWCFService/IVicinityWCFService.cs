2d001862b293e4f2149be5ac6135aab18e874ff6
Nikolaj Čolić <nikolaj.colic@gorenje.com>
2019-07-01 11:12:14 +0200

    Update IVicinityWCFService.cs

--- vicinityCLP/VicinityWCFService/IVicinityWCFService.cs
+++ vicinityCLP/VicinityWCFService/IVicinityWCFService.cs
@@ -1,4 +1,11 @@
-﻿using System.IO;
+/**
+Copyright © 2019 Gorenje gospodinjski aparati, d.o.o.. All rights reserved.
+This file is part of vicinity-adapter-GRN.
+#component# is free software: you can redistribute it and/or modify it under the terms of GNU General Public License v3.0.
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT ANY WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT, IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+See README file for the full disclaimer information and LICENSE file for full license information in the project root.
+**/
+using System.IO;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 
