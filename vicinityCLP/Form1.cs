a09e3e3ba9d9f4007292c2a21b57e9a5be31e4bc
Nikolaj Čolić <nikolaj.colic@gorenje.com>
2019-07-01 10:43:15 +0200

    Update Form1.cs

--- vicinityCLP/Form1.cs
+++ vicinityCLP/Form1.cs
@@ -1,7 +1,7 @@
 /**
-Copyright © #YYYY#[1] Company. All rights reserved.
-This file is part of #component#.
-#component# is free software: you can redistribute it and/or modify it under the terms of #license#.
+Copyright © 2019 Gorenje gospodinjski aparati, d.o.o.. All rights reserved.
+This file is part of vicinity-adapter-GRN.
+#component# is free software: you can redistribute it and/or modify it under the terms of GNU General Public License v3.0.
 THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT ANY WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT, IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 See README file for the full disclaimer information and LICENSE file for full license information in the project root.
 **/
