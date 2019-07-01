edfc2b6b866e2c637dbe5bd55875905b004ea432
Nikolaj Čolić <nikolaj.colic@gorenje.com>
2019-07-01 10:39:43 +0200

    Update Form1.cs

--- vicinityCLP/Form1.cs
+++ vicinityCLP/Form1.cs
@@ -1,4 +1,11 @@
-﻿using System;
+/**
+Copyright © #YYYY#[1] Company. All rights reserved.
+This file is part of #component#.
+#component# is free software: you can redistribute it and/or modify it under the terms of #license#.
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT ANY WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT, IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+See README file for the full disclaimer information and LICENSE file for full license information in the project root.
+**/
+using System;
 using System.Diagnostics;
 using System.Drawing;
 using System.Threading.Tasks;
